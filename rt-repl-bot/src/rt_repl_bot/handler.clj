aa98945912a75b1239c4c7135a70ce532a233025
Alex Simkin <asimkin@atlassian.com>
2016-11-09 17:22:50 +1100

    fixes and improvements. API compliance.

--- rt-repl-bot/src/rt_repl_bot/handler.clj
+++ rt-repl-bot/src/rt_repl_bot/handler.clj
@@ -6,17 +6,22 @@
 
 (defn handle-command [command]
   (try
-    {:status 401
-     :body (eval (read-string command))}
-    (catch Exception e {:status 401
-                        :body e})))
+    (let [result (eval (read-string command))]
+      {:status 401
+       :body   {:text (str "<span>" (prn-str result) "</span>")
+                :bot  "REPL-bot"}})
+    (catch Exception e
+      (let [message (.getMessage e) stacktrace (str/join "\n\t" (map str (.getStackTrace e)))]
+        {:status 400
+         :body   {:text (str "<span>" message "\n" stacktrace "</span>")
+                  :bot  "REPL-bot"}}))))
 
 (defroutes app-routes
-  (POST "/event" request
-    (let [text (or (get-in request [:body "text"]) "")]
-      (cond
-        (str/starts-with? text "clj>")  (handle-command (subs text 4))
-        :else {:status 417}))))
+           (POST "/event" request
+             (let [text (or (get-in request [:body "text"]) "")]
+               (cond
+                 (str/starts-with? text "clj>") (handle-command (subs text 4))
+                 :else {:status 417}))))
 
 (def app
   (->
