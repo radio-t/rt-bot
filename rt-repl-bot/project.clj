9c65f5b4dceb19acef743297dff890975a81a20f
Alex Simkin <asimkin@atlassian.com>
2016-11-10 10:44:24 +1100

    handle infinite loops and long running computations

--- rt-repl-bot/project.clj
+++ rt-repl-bot/project.clj
@@ -1,6 +1,7 @@
 (defproject rt-repl-bot "0.1.0-SNAPSHOT"
   :description "Clojure REPL bot for Radio-T chat"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [compojure "1.5.1"]
                  [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.9.7"]]
--- rt-repl-bot/src/rt_repl_bot/handler.clj
+++ rt-repl-bot/src/rt_repl_bot/handler.clj
@@ -1,7 +1,24 @@
 (ns rt-repl-bot.handler
   (:use compojure.core ring.middleware.json)
+  (:import (java.util.concurrent TimeUnit TimeoutException FutureTask))
   (:require [clojure.string :as str]
-            [compojure.handler :as handler]))
+            [clojure.tools.logging :as log]
+            [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defmacro with-timeout [millis & body]
+  `(let [task# (FutureTask. (fn [] ~@body))
+         thread# (Thread. task# "repl-thread")]
+     (.setDaemon thread# true)
+     (try
+       (.start thread#)
+       (.get task# ~millis TimeUnit/MILLISECONDS)
+       (catch TimeoutException _#
+         (log/warn "Evaluation timed out in 5 seconds")
+         (.cancel task# true)
+         "Evaluation timed out in 5 seconds")
+       (finally
+         (.stop thread#)))))
 
 (defn eval-command [command]
   (with-out-str
@@ -10,17 +27,20 @@
       (catch Exception e (print (.getMessage e))))))
 
 (defn handle-command [command]
-  {:status 201
-   :body   {:text (str "`" (eval-command command) "`")
-            :bot  "REPL-bot"}
-   })
+  (let [eval-result (with-timeout 5000 (eval-command command))]
+    (log/info "clj>" command " ~ " eval-result)
+    {:status 201
+     :body   {:text (str "`" eval-result "`")
+              :bot  "REPL-bot"}
+     }))
 
 (defroutes app-routes
            (POST "/event" request
              (let [text (or (get-in request [:body "text"]) "")]
                (cond
                  (str/starts-with? text "clj>") (handle-command (subs text 4))
-                 :else {:status 417}))))
+                 :else {:status 417})))
+           (route/not-found {:status 404}))
 
 (def app
   (->
