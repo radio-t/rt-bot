3a46449786614336d4000dbcdd33968bad8d5c47
Igor <mcfalu@gmail.com>
2016-11-12 16:50:07 +0200

    Fixed word misspelling

--- gif-bot/config.go
+++ gif-bot/config.go
@@ -6,6 +6,6 @@ const (
 	MaxAttemps     = 10
 	UrlRandomItem  = "http://developerslife.ru/random?json=true"
 	BotAuthor      = "Igor Yatsenko (https://twitter.com/IgorYatsenko)"
-	BotDescription = "Бог генерирует случайную gif картинку"
+	BotDescription = "Бот генерирует случайную gif картинку"
 	BotCommand     = "гифка!"
 )
