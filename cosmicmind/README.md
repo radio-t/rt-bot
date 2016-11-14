ed6ae54328d89273023a8429c4ca9cd66bbb7812
Andrew Scorpil <dev@scorpil.com>
2016-11-14 21:11:54 +0100

    Finish cosmicmind

--- cosmicmind/README.md
+++ cosmicmind/README.md
@@ -1,6 +1,19 @@
-# Радио-Т пример бота
-https://github.com/umputun/rt-bot
+# Cosmicmind бот
+Связывается с вселенским интелектом чтобы узнать ответит на любой вопрос.
+Решить такую задачу можно только на Haskell, конечно же.
 
-Этот бот не настоящий, просто для примера с целью помочь авторам, если у них вопросы по API. Он не будет активен в ральной жизни
+## Пример
 
-Бот слушает все сообщения и отвечает на любое ревeрсным текстом.
+	> http get localhost:8080/event text="cosmicmind, камера щелкает динамиком?" username:=123 display_name="someuser"
+	HTTP/1.1 201 Created
+	Content-Type: application/json; charset=utf-8
+	Date: Mon, 14 Nov 2016 20:04:59 GMT
+	Server: Warp/3.2.9
+	Transfer-Encoding: chunked
+
+	{
+		"name": "Cosmicmind",
+		"text": "someuser, oтвет на ваш вопрос \"cosmicmind, щелкает динамиком?\": Нет."
+	}
+
+Q.E.D.
--- cosmicmind/src/Main.hs
+++ cosmicmind/src/Main.hs
@@ -1,14 +1,61 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 import qualified Data.Map as Map
+import Control.Monad.Trans
+import Data.Aeson (FromJSON)
+import Data.List (isInfixOf)
+import Data.Char (toLower, toUpper)
+import GHC.Generics (Generic)
+import Network.HTTP.Types.Status (created201, expectationFailed417)
 import Web.Scotty
-import Data.Monoid (mconcat)
+import System.Random
+
+data ChatMsg = ChatMsg { text :: String
+                       , username :: Integer
+                       , display_name :: String
+                       } deriving (Show, Generic)
+
+instance FromJSON ChatMsg
+
+botName = "cosmicmind"
+
+respond :: String -> String -> ActionM()
+respond user question = do
+  answer <- liftIO cosmicmindSays
+  json $ Map.fromList ([ ("text", sayAnswer user question answer)
+                       , ("name", capitalize botName)] :: [(String, String)])
+  where
+    capitalize :: String -> String
+    capitalize inp = (toUpper (head inp)):(tail inp)
+
+    cosmicmindSays :: IO Bool
+    cosmicmindSays = do
+      r <- randomIO :: IO Double
+      return(r > 0.5)
+
+    sayAnswer :: String -> String -> Bool -> String
+    sayAnswer user question answer =
+      user ++ ", oтвет на ваш вопрос \"" ++ question ++ "\": " ++
+      (if answer then "Да" else "Нет") ++ "."
+
 
 main = scotty 8080 $ do
   get "/info" $ do
-    json (Map.fromList info)
-    where
+    let
       info ::[(String, String)]
-      info = [
-        ("author","Scorpil"),
-        ("info", "Answers all questions.")]
+      info = [ ("author","Scorpil")
+             , ("info", "Answers all questions.")]
+    json $ Map.fromList info
 
+  get "/event" $ do
+    t <- jsonData
+    let
+      question = Main.text t
+      user = Main.display_name t
+    if isInteresting question
+      then status created201 >> respond user question
+      else status expectationFailed417
+    where
+      isInteresting :: String -> Bool
+      isInteresting question =
+        (botName `isInfixOf` (map toLower question)) && (last question) == '?'
