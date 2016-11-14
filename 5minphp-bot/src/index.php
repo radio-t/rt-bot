d3e11a8df4cf2c612a1a56af18df25b7c78de853
Petr Myazin <petr.myazin@gmail.com>
2016-11-14 13:55:29 +0300

    Убрал проверку на собственные сообщения, убрал ссылку на twitter из /info (gitter разворачивал), добавил обработку ошибок rollbar

--- 5minphp-bot/src/index.php
+++ 5minphp-bot/src/index.php
@@ -1,4 +1,15 @@
 <?php
+require_once __DIR__ . '/rollbar.php';
+// set-up error handling via rollbar
+Rollbar::init([
+    // required
+    'access_token' => '916b71816d754c3abd77b300d4d9941c',
+    // sets whether errors suppressed with '@' should be reported or not
+    'report_suppressed' => true,
+    // optional - path to directory your code is in. used for linking stack traces.
+    'root' => __DIR__
+]);
+
 define('LOCAL_FILE', __DIR__ . '/phpfact.txt');
 define('REMOTE_FILE', 'https://raw.githubusercontent.com/pqr/5minphp-bot/master/phpfact.txt');
 define('BOT_NAME', 'Пятиминутка PHP');
@@ -6,8 +17,8 @@ define('BOT_NAME', 'Пятиминутка PHP');
 if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] === '/info') {
     http_response_code(200);
     print json_encode([
-        'author' => 'Пётр Мязин (https://twitter.com/PetrMyazin)',
-        'info' => 'Если в чате упоминается PHP, то "Пятиминутка PHP" сообщает какой-то интересный и полезный факт об этом языке программирования! База знаний постоянно пополняется!',
+        'author' => 'Пётр Мязин',
+        'info' => 'бот "Пятиминутка PHP" расскажет интересный и полезный факт об этом языке программирования, при упоминании PHP. Twitter: 5minphp',
         'commands' => []
     ]);
     exit();
@@ -19,29 +30,16 @@ if (!$input) {
     exit();
 }
 
-$inputData = json_decode($input, true);
+$inputData = @json_decode($input, true);
 if (!$inputData || !isset($inputData['text']) || !is_string($inputData['text'])) {
     http_response_code(417);
     exit();
 }
 
-$username = $inputData['username'] ?? '';
-$displayname = $inputData['display_name'] ?? '';
-if ($username === BOT_NAME
-    || $displayname === BOT_NAME
-    || $username === 'rt-bot'
-    || $displayname === 'rt-bot'
-    || mb_stripos($inputData['text'], BOT_NAME) !== false) {
-
-    // without test chat it's not clear yet how messagees from the bot will look like
-    // so trying to check all possible conditions to prevent reply on self messages
-    http_response_code(417);
-    exit();
-}
-
-if (mb_stripos($inputData['text'], 'пятиминутка обновись') !== false) {
+if (mb_stripos($inputData['text'], 'пятиминутка, обновись') !== false) {
     http_response_code(201);
-    print json_encode(['text' => 'У меня для вас есть свежие факты про PHP...', 'bot' => BOT_NAME]);
+    downloadFacts();
+    print json_encode(['text' => 'У меня для вас есть свежие факты про PHP. Просто напишите сообщение с текстом содержащим "PHP", и вы узнаете, что...', 'bot' => BOT_NAME]);
     exit();
 }
 
@@ -75,7 +73,7 @@ function hasWordPhp($inputText): bool
 
 function getRandomFact(string $inputData): string
 {
-    if (!file_exists(LOCAL_FILE) || time() - @filemtime(LOCAL_FILE) > 3600) {
+    if (!@file_exists(LOCAL_FILE) || time() - @filemtime(LOCAL_FILE) > 3600) {
         downloadFacts();
     }
 
