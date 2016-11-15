06794a556f3d2ddfe14f2ec88ee1e29083d0a656
terbooter <terbooter@gmail.com>
2016-11-15 21:43:04 +0300

    экранирование переноса строки

--- anek-bot/app/AnekdotData.js
+++ anek-bot/app/AnekdotData.js
@@ -8,7 +8,7 @@ var AnekdotData = (function () {
         anekdot.title = o.title[0];
         anekdot.pubDate = o.pubDate[0];
         anekdot.description = o.description[0];
-        anekdot.description = anekdot.description.replace(/<br>/g, " \n ");
+        anekdot.description = anekdot.description.replace(/<br>/g, "  \\n");
         return anekdot;
     };
     AnekdotData.getDefaultAnekdot = function () {
