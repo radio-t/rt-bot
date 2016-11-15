75c2d5ed2c52376f9f2aee406aef724618b046c4
Sergey Levitin <selevit@gmail.com>
2016-11-16 01:18:13 +0300

    Update .travis.yml

--- ci/run_tests_in_docker.sh
+++ ci/run_tests_in_docker.sh
@@ -4,6 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONTAINER_NAME="${CONTAINER_NAME:-rt-bot/checker}"
 docker run --rm -t --volume="$DIR/..:/project:ro" \
     -e BOT_BASE_URL="$BOT_BASE_URL" \
-    -e LENSENCRYPT=false \
     "$CONTAINER_NAME" /project
 _
