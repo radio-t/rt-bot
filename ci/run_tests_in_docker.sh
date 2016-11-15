3162ee8e3943fc0f96a779f8a534a25df9ecab9a
Sergey Levitin <selevit@gmail.com>
2016-11-16 01:01:39 +0300

    Add ci tests for bots

---
+++ ci/run_tests_in_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONTAINER_NAME="${CONTAINER_NAME:-rt-bot/checker}"
+docker run --rm -t --volume="$DIR/..:/project:ro" \
+    -e BOT_BASE_URL="$BOT_BASE_URL" \
+    -e LENSENCRYPT=false \
+    "$CONTAINER_NAME" /project
+_
