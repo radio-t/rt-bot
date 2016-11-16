94c3776a9709e9319587141af516d4ccd7aa846a
Your Name <you@example.com>
2016-11-16 11:20:39 +0800

    clean up

--- grabpage-bot/test_parse.py
+++
@@ -1,9 +0,0 @@
-import re
-
-# url = '<p>Hello World</p><a href="http://example.com">More Examples</a><a href="http://example2.com">Even More Examples</a>'
-
-url = '	Firebase http://www.javarepl.com/# angularjs,firebase'
-
-urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', url)
-
-print(urls)
