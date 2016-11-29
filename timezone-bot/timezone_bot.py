c3ea11a9236547b03430265c560f47cbb2f4a028
Anton Prokhorov <betrayer11@gmail.com>
2016-11-29 22:47:17 +0700

    Add function to get local time of city from request
    Function to getting local time from passed json in request has been
    added. Request should be a valid JSON, which contains ``text`` field.
    If ``text`` contains key phrase "время в/во {city_name}" then local
    time would be calculated through google maps API python wrapper.
    Otherwise 417 status code will be raised

--- timezone-bot/timezone_bot.py
+++ timezone-bot/timezone_bot.py
@@ -1,9 +1,25 @@
+import datetime
+import os
+import re
 from json.decoder import JSONDecodeError
 
+import googlemaps
 from flask import Flask, json, request
+from pytz import timezone
 from werkzeug.exceptions import ExpectationFailed
 
 app = Flask(__name__)
+
+google_key = os.environ.get('GOOGLE_KEY')
+
+if not google_key:
+    raise ValueError('GOOGLE_KEY environment variable is not set')
+
+gmaps_api = googlemaps.Client(google_key)
+
+fmt = '%d.%m.%Y %H:%M:%S'
+
+bot_name = 'timezone_bot'
 
 
 @app.route('/event', methods=['POST'])
@@ -12,10 +28,46 @@ def event():
     try:
         message = json.loads(request.data).get('text', None)
 
-        response = {
-            'text': 'Hello world',
-            'bot': 'timezone_bot'
-        }
-        return response, 201
+        if not message:
+            return ExpectationFailed()
+
+        # Check if message matches with pattern
+        result = re.findall(r'время (в|во) (.*)', message)
+
+        if not result:
+            return ExpectationFailed()
+
+        city = result[0][1]
+
+        # Get places by city name (``query`` arg for ``places`` method)
+        places = gmaps_api.places(city)
+
+        if places['status'] == 'OK':
+            # Get timezone by location
+            tz = gmaps_api.timezone(
+                places['results'][0]['geometry']['location']
+            )
+            if tz['status'] == 'OK':
+                # Load timezone
+                location = timezone(tz['timeZoneId'])
+                localized_time = datetime.datetime.now(location)
+                return json.dumps({
+                    'text': 'Местное время в {city} сейчас {time}'.format(
+                        city=city,
+                        time=localized_time.strftime(fmt)
+                    ),
+                    'bot': bot_name
+                }, ensure_ascii=False), 201
+            else:
+                return json.dumps({
+                    'text': 'Не могу получить данные о часовом поясе',
+                    'bot': bot_name
+                }, ensure_ascii=False), 201
+        else:
+            return json.dumps({
+                'text': 'Не могу найти город {city}'.format(city=city),
+                'bot': bot_name
+            }, ensure_ascii=False), 201
+
     except JSONDecodeError:
         return ExpectationFailed()
