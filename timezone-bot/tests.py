5e4103c0471bcd4a174765e8694730f15e8e8371
Anton Prokhorov <betrayer11@gmail.com>
2016-11-30 20:14:11 +0700

    Refactor tests

--- timezone-bot/tests.py
+++ timezone-bot/tests.py
@@ -13,6 +13,23 @@ class TimeZoneBotTestCase(TestCase):
         self.client = app.test_client()
 
         self.url = '/event'
+        self.places = {
+            'status': 'OK',
+            'results': [
+                {
+                    'geometry': {
+                        'location': 'Location'
+                    }
+                }
+            ]
+        }
+        self.timezone = {
+            'status': 'OK',
+            'timeZoneId': 'Asia/Krasnoyarsk'
+        }
+        self.empty_data = json.dumps({})
+        self.invalid_data = json.dumps({'text': 'Hello world'})
+        self.valid_data = json.dumps({'text': 'время в красноярске'})
 
     def test_event_with_no_data_sent(self):
         """Ensure that 417 status code returns when no data sent"""
@@ -21,13 +38,12 @@ class TimeZoneBotTestCase(TestCase):
 
     def test_event_with_empty_dict_sent(self):
         """Ensure that 417 status code returns when empty dict sent"""
-        resp = self.client.post(self.url, data=json.dumps({}))
+        resp = self.client.post(self.url, data=self.empty_data)
         self.assertEquals(resp.status_code, 417)
 
     def test_some_random_message(self):
         """Ensure that bot will not say if shouldn't"""
-        resp = self.client.post(self.url,
-                                data=json.dumps({'text': 'Hello world'}))
+        resp = self.client.post(self.url, data=self.invalid_data)
         self.assertEquals(resp.status_code, 417)
 
     @mock.patch('googlemaps.Client.places')
@@ -35,62 +51,39 @@ class TimeZoneBotTestCase(TestCase):
         """Ensure that bot will try to answer if 'время в/во <cityname>'
         obtained from request data
         """
-        resp = self.client.post(self.url, data=json.dumps({
-            'text': 'время в красноярске'
-        }))
+        resp = self.client.post(self.url, data=self.valid_data)
         self.assertEquals(resp.status_code, 201)
         places.assert_called_with('красноярске')
         dict_data = json.loads(resp.data)
         self.assertEquals(dict_data['text'], 'Не могу найти город красноярске')
 
-    @mock.patch('googlemaps.Client.places', return_value={
-        'status': 'OK',
-        'results': [
-            {
-                'geometry': {
-                    'location': 'Location'
-                }
-            }
-        ]
-    })
+    @mock.patch('googlemaps.Client.places')
     @mock.patch('googlemaps.Client.timezone')
     def test_problems_with_timezone(self, timezone, places):
         """Ensure that if timezone is not loaded correctly, bot answers that
         can't obtain and load timezone data
         """
-        resp = self.client.post(self.url, data=json.dumps({
-            'text': 'время в красноярске'
-        }))
+        places.return_value = self.places
+        resp = self.client.post(self.url, data=self.valid_data)
         timezone.assert_called_with('Location')
         self.assertEquals(resp.status_code, 201)
         dict_data = json.loads(resp.data)
         self.assertEquals(dict_data['text'], 'Не могу получить данные о '
                                              'часовом поясе')
 
-    @mock.patch('googlemaps.Client.places', return_value={
-        'status': 'OK',
-        'results': [
-            {
-                'geometry': {
-                    'location': 'Location'
-                }
-            }
-        ]
-    })
-    @mock.patch('googlemaps.Client.timezone', return_value={
-        'status': 'OK',
-        'timeZoneId': 'Asia/Krasnoyarsk'
-    })
+    @mock.patch('googlemaps.Client.places')
+    @mock.patch('googlemaps.Client.timezone')
     def test_timezone_obtained(self, timezone, places):
         """Ensure that if timezone correctly obtained, bot says time in this
         timezone
         """
-        resp = self.client.post(self.url, data=json.dumps({
-            'text': 'время в красноярске'
-        }))
+        places.return_value = self.places
+        timezone.return_value = self.timezone
+        resp = self.client.post(self.url, data=self.valid_data)
         self.assertEquals(resp.status_code, 201)
         dict_data = json.loads(resp.data)
         self.assertIn('Местное время в красноярске сейчас', dict_data['text'])
+
 
 if __name__ == '__main__':
     unittest.main()
