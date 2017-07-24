4e2ce2f04ecffcd3cfe3649e979a9ce1bc996941
strayge <stray.omsk@gmail.com>
2017-07-24 20:59:14 +0600

    money-bot: added BTC

--- money-bot/app.js
+++ money-bot/app.js
@@ -15,7 +15,8 @@ const currency_RegExp = {
     e: /(((€|eur)\s?)((\d+\s\d+\s\d+|\d+\s\d+|\d+)((,|\.)\d+)?)(\s?(миллион(а|ов)?|миллиард(а|ов)?|млн|млрд|тысячи|тыс|тысяч|сотен|сотни|k|к)\.?\s?)?|((\d+\s\d+\s\d+|\d+\s\d+|\d+)((,|\.)\d+)?(\s?(миллион(а|ов)?|миллиард(а|ов)?|млн|млрд|тысячи|тыс|тысяч|сотен|сотни|k|к)\.?\s?)?(\s?(€|евро|эвро|eur|ewro))))/i,
     h: /(((₴|uah)\s?)((\d+\s\d+\s\d+|\d+\s\d+|\d+)((,|\.)\d+)?)(\s?(миллион(а|ов)?|миллиард(а|ов)?|млн|млрд|тысячи|тыс|тысяч|сотен|сотни|k|к)\.?\s?)?|((\d+\s\d+\s\d+|\d+\s\d+|\d+)((,|\.)\d+)?(\s?(миллион(а|ов)?|миллиард(а|ов)?|млн|млрд|тысячи|тыс|тысяч|сотен|сотни|k|к)\.?\s?)?(\s?(₴|грн|гривень|гривны|hrn|hriven|uah))))/i,
     r: /(((₽|rub)\s?)((\d+\s\d+\s\d+|\d+\s\d+|\d+)((,|\.)\d+)?)(\s?(миллион(а|ов)?|миллиард(а|ов)?|млн|млрд|тысячи|тыс|тысяч|сотен|сотни|k|к)\.?\s?)?|((\d+\s\d+\s\d+|\d+\s\d+|\d+)((,|\.)\d+)?(\s?(миллион(а|ов)?|миллиард(а|ов)?|млн|млрд|тысячи|тыс|тысяч|сотен|сотни|k|к)\.?\s?)?(\s?(₽|руб|rub))))/i,
-    b: /(((Br|BYN)\s?)((\d+\s\d+\s\d+|\d+\s\d+|\d+)((,|\.)\d+)?)(\s?(миллион(а|ов)?|миллиард(а|ов)?|млн|млрд|тысячи|тыс|тысяч|сотен|сотни|k|к)\.?\s?)?|((\d+\s\d+\s\d+|\d+\s\d+|\d+)((,|\.)\d+)?(\s?(миллион(а|ов)?|миллиард(а|ов)?|млн|млрд|тысячи|тыс|тысяч|сотен|сотни|k|к)\.?\s?)?(\s?(Br|белорусских рублей|белорусских|BYN))))/i
+    b: /(((Br|BYN)\s?)((\d+\s\d+\s\d+|\d+\s\d+|\d+)((,|\.)\d+)?)(\s?(миллион(а|ов)?|миллиард(а|ов)?|млн|млрд|тысячи|тыс|тысяч|сотен|сотни|k|к)\.?\s?)?|((\d+\s\d+\s\d+|\d+\s\d+|\d+)((,|\.)\d+)?(\s?(миллион(а|ов)?|миллиард(а|ов)?|млн|млрд|тысячи|тыс|тысяч|сотен|сотни|k|к)\.?\s?)?(\s?(Br|белорусских рублей|белорусских|BYN))))/i,
+    btc: /(((BTC)\s?)((\d+\s\d+\s\d+|\d+\s\d+|\d+)((,|\.)\d+)?)(\s?(миллион(а|ов)?|миллиард(а|ов)?|млн|млрд|тысячи|тыс|тысяч|сотен|сотни|k|к)\.?\s?)?|((\d+\s\d+\s\d+|\d+\s\d+|\d+)((,|\.)\d+)?(\s?(миллион(а|ов)?|миллиард(а|ов)?|млн|млрд|тысячи|тыс|тысяч|сотен|сотни|k|к)\.?\s?)?(\s?(BTC|биткоин(а|ов)?|биток|битков))))/i
 };
 let currency = {
     USD_EUR: 0,
@@ -42,7 +43,12 @@ let currency = {
     BYN_EUR: 0,
     BYN_UAH: 0,
     BYN_RUB: 0,
-    BYN_BTC: 0
+    BYN_BTC: 0,
+    BTC_USD: 0,
+    BTC_EUR: 0,
+    BTC_UAH: 0,
+    BTC_RUB: 0,
+    BTC_BYN: 0
 };
 
 
@@ -77,11 +83,18 @@ app.post('/event', function(req, res) {
                 c = "BYN";
                 callback(null, v, c);
             }
+            else if(text.match(currency_RegExp.btc)!=null){
+                v = text.match(currency_RegExp.btc)[0];
+                c = "BTC";
+                callback(null, v, c);
+            }
             else callback(true);
         },
         function(v, c, callback){
             // remove keywords with 'к' symbols
             v = v.replace(/бакс/gi, '')
+            v = v.replace(/(биткоин(а|ов)?|биток|битков)/gi, '')
+
             let val = v.replace(/[^\d(,|\.)-]/g, '');
             val = parseFloat(val);
             if(~v.indexOf("k")) val = val * 1000;
@@ -102,84 +115,119 @@ app.post('/event', function(req, res) {
                 res_2 = 0,
                 res_3 = 0,
                 res_4 = 0,
+                res_5 = 0,
                 cur_1 = 0,
                 cur_2 = 0,
                 cur_3 = 0,
                 cur_4 = 0,
+                cur_5 = 0,
                 name_1 = 0,
                 name_2 = 0,
                 name_3 = 0,
-                name_4 = 0;
+                name_4 = 0,
+                name_5 = 0;
 
             if(c=="USD"){
                 res_1 = ((currency.USD_EUR*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_2 = ((currency.USD_UAH*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_3 = ((currency.USD_RUB*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_4 = ((currency.USD_BYN*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
+                res_5 = ((currency.USD_BTC*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 cur_1 = currency.USD_EUR;
                 cur_2 = currency.USD_UAH;
                 cur_3 = currency.USD_RUB;
                 cur_4 = currency.USD_BYN;
+                cur_5 = currency.USD_BTC;
                 name_1 = "EUR";
                 name_2 = "UAH";
                 name_3 = "RUB";
                 name_4 = "BYN";
+                name_5 = "BTC";
             }
             else if(c=="EUR"){
                 res_1 = ((currency.EUR_USD*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_2 = ((currency.EUR_UAH*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_3 = ((currency.EUR_RUB*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_4 = ((currency.EUR_BYN*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
+                res_5 = ((currency.EUR_BTC*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 cur_1 = currency.EUR_USD;
                 cur_2 = currency.EUR_UAH;
                 cur_3 = currency.EUR_RUB;
                 cur_4 = currency.EUR_BYN;
+                cur_5 = currency.EUR_BTC;
                 name_1 = "USD";
                 name_2 = "UAH";
                 name_3 = "RUB";
                 name_4 = "BYN";
+                name_5 = "BTC";
             }
             else if(c=="UAH"){
                 res_1 = ((currency.UAH_USD*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_2 = ((currency.UAH_EUR*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_3 = ((currency.UAH_RUB*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_4 = ((currency.UAH_BYN*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
+                res_5 = ((currency.UAH_BTC*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 cur_1 = currency.UAH_USD;
                 cur_2 = currency.UAH_EUR;
                 cur_3 = currency.UAH_RUB;
                 cur_4 = currency.UAH_BYN;
+                cur_5 = currency.UAH_BTC;
                 name_1 = "USD";
                 name_2 = "EUR";
                 name_3 = "RUB";
                 name_4 = "BYN";
+                name_5 = "BTC";
             }
             else if(c=="RUB"){
                 res_1 = ((currency.RUB_USD*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_2 = ((currency.RUB_EUR*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_3 = ((currency.RUB_UAH*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_4 = ((currency.RUB_BYN*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
+                res_5 = ((currency.RUB_BTC*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 cur_1 = currency.RUB_USD;
                 cur_2 = currency.RUB_EUR;
                 cur_3 = currency.RUB_UAH;
                 cur_4 = currency.RUB_BYN;
+                cur_5 = currency.RUB_BTC;
                 name_1 = "USD";
                 name_2 = "EUR";
                 name_3 = "UAH";
                 name_4 = "BYN";
+                name_5 = "BTC";
             }
             else if(c=="BYN"){
                 res_1 = ((currency.BYN_USD*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_2 = ((currency.BYN_EUR*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_3 = ((currency.BYN_UAH*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 res_4 = ((currency.BYN_RUB*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
+                res_5 = ((currency.BYN_BTC*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
                 cur_1 = currency.BYN_USD;
                 cur_2 = currency.BYN_EUR;
                 cur_3 = currency.BYN_UAH;
                 cur_4 = currency.BYN_RUB;
+                cur_5 = currency.BYN_BTC;
                 name_1 = "USD";
                 name_2 = "EUR";
                 name_3 = "UAH";
                 name_4 = "RUB";
+                name_5 = "BTC";
+            }
+            else if(c=="BTC"){
+                res_1 = ((currency.BTC_USD*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
+                res_2 = ((currency.BTC_EUR*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
+                res_3 = ((currency.BTC_UAH*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
+                res_4 = ((currency.BTC_RUB*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
+                res_5 = ((currency.BTC_BYN*parseFloat(v)).toFixed(2).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ')).replace(".00", "");
+                cur_1 = currency.BTC_USD;
+                cur_2 = currency.BTC_EUR;
+                cur_3 = currency.BTC_UAH;
+                cur_4 = currency.BTC_RUB;
+                cur_5 = currency.BTC_BYN;
+                name_1 = "USD";
+                name_2 = "EUR";
+                name_3 = "UAH";
+                name_4 = "RUB";
+                name_5 = "BYN";
             }
 
             let responseText = "**_"+req.body.username+"_** упомянул " + v + " "+ c + "\\n\\n"+
@@ -188,7 +236,8 @@ app.post('/event', function(req, res) {
                 "| "+name_1+" | "+res_1+" | "+cur_1.toFixed(2)+" |\\n"+
                 "| "+name_2+" | "+res_2+" | "+cur_2.toFixed(2)+" |\\n"+
                 "| "+name_3+" | "+res_3+" | "+cur_3.toFixed(2)+" |\\n"+
-                "| "+name_4+" | "+res_4+" | "+cur_4.toFixed(2)+" |";
+                "| "+name_4+" | "+res_4+" | "+cur_4.toFixed(2)+" |\\n"+
+                "| "+name_5+" | "+res_5+" | "+cur_5.toFixed(2)+" |";
 
             callback(null, responseText);
         }
@@ -207,7 +256,7 @@ app.post('/event', function(req, res) {
 app.all('/info', function(req, res) {
     res.json({
         author: 'exelban',
-        info: 'Если в сообщение была упомянутая какая-то валюта (USD, EUR, UAH, RUB), бот конвертирует ее в доллары, евро, грн, руб.'
+        info: 'Если в сообщение была упомянутая какая-то валюта (USD, EUR, UAH, RUB, BTC), бот конвертирует ее в доллары, евро, грн, руб.'
     });
     res.end();
 });
@@ -275,6 +324,12 @@ function get_currency(){
                     currency.BTC_UAH = currency.BTC_USD * currency.USD_UAH;
                     currency.BTC_RUB = currency.BTC_USD * currency.USD_RUB;
                     currency.BTC_BYN = currency.BTC_USD * currency.USD_BYN;
+                    currency.EUR_BTC = 1 / currency.BTC_EUR;
+                    currency.UAH_BTC = 1 / currency.BTC_UAH;
+                    currency.RUB_BTC = 1 / currency.BTC_RUB;
+                    currency.BYN_BTC = 1 / currency.BTC_BYN;
+
+                    console.log('currencies updated.')
 
                     callback(null);
                 }
