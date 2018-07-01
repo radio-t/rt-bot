622a0c71626b1fb7e6502f1eabd73adee45cd91c
strayge <stray.omsk@gmail.com>
2018-07-01 19:46:21 +0600

    tweet-bot: moved to twitter api from html parsing

--- tweet-bot/main.py
+++ tweet-bot/main.py
@@ -2,7 +2,7 @@ import json.decoder
 import logging
 import re
 
-import requests
+import tweepy
 from aiohttp import web
 
 logging.basicConfig(format='%(asctime)-15s %(levelname)-8s %(message)s', level=logging.INFO,
@@ -12,43 +12,7 @@ log = logging.getLogger()
 aiohttp_logger = logging.getLogger('aiohttp')
 aiohttp_logger.setLevel(logging.WARNING)
 
-requests_session = None
-twitter_login_endpoint = 'https://twitter.com/sessions'
-
-
-def create_requests_session():
-    log.info('creating twitter session...')
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0'}
-    req_session = requests.Session()
-    req_session.headers.update(headers)
-    req_main_page = req_session.get('https://twitter.com/', timeout=20)
-    if 'name="authenticity_token" value="' in req_main_page.text:
-        authenticity_token = req_main_page.text.split('name="authenticity_token" value="', 1)[1].split('"', 1)[0]
-    elif '" name="authenticity_token">' in req_main_page.text:
-        authenticity_token = req_main_page.text.split('" name="authenticity_token">', 1)[0].split('"')[-1]
-    else:
-        log.warning('main page without authenticity_token')
-        return
-    username = 'tweetbotrt'
-    password = 'LEIa8BwGKWe87mZvmxEu'
-    data = {'session[username_or_email]': username,
-            'session[password]': password,
-            'authenticity_token': authenticity_token,
-            'remember_me': '1',
-            'return_to_ssl': 'true',
-            'redirect_after_login': '/',
-            }
-    req_login = req_session.post(twitter_login_endpoint, data=data, timeout=20)
-    if twitter_login_endpoint in req_login.text:
-        log.warning('login failed')
-        return
-    return req_session
-
-
-def is_login_page(req):
-    if twitter_login_endpoint in req.text:
-        return True
-    return False
+api = None
 
 
 async def http_info(request):
@@ -60,20 +24,22 @@ async def http_info(request):
 
 def get_last_tweet_id(username):
     try:
-        global requests_session
-        if not requests_session:
+        global api
+        if not api:
             log.info('session not founded')
-            requests_session = create_requests_session()
-        r = requests_session.get('https://twitter.com/%s/with_replies' % username, timeout=20)
-        if is_login_page(r):
-            log.warning('login page returned')
-            requests_session = None
-            return
-        if r.status_code != 200 or 'data-tweet-id=' not in r.text:
+            consumer_key = '07jCKabcyPVjBoYH0FzDsR271'
+            consumer_secret = 'gzOPw2tZymbzUJjRdisDsXOaFRc8YORReb9J1gCk43yyPZ0Vnf'
+            access_token = '878737768146513922-RSavn9pgCKxNkjnxA3qvs3OeGlQrb5D'
+            access_token_secret = '5JX6HBK9XUHvLsxTyMRGHABQ1IUVqFwzaM3sOQPaI2zhv'
+            auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+            auth.set_access_token(access_token, access_token_secret)
+            api = tweepy.API(auth)
+
+        timeline = api.user_timeline(username, count=1)
+        if not timeline:
             log.warning('no tweets founded')
             return
-        last_id = r.text.split('data-tweet-id="', 1)[1].split('"', 1)[0]
-        return last_id
+        return timeline[0].id
     except:
         return
 
@@ -81,7 +47,7 @@ def get_last_tweet_id(username):
 def get_last_tweet_link(username):
     last_tweet_id = get_last_tweet_id(username)
     if last_tweet_id:
-        text = 'https://twitter.com/%s/status/%s' % (username, str(last_tweet_id))
+        text = 'https://twitter.com/{}/status/{}'.format(username, last_tweet_id)
     else:
         text = 'Что-то пошло не так :disappointed: '
     return text
